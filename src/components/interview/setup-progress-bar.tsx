333826b362d2848d07660400b079dde067c0b701
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-11-01 16:17:43 +0900

    DV-53 design: 프로그레스바 디자인 변경

--- src/components/interview/setup-progress-bar.tsx
+++ src/components/interview/setup-progress-bar.tsx
@@ -1,3 +1,5 @@
+import { FaCheck } from "react-icons/fa"; // 체크 아이콘을 위한 import
+
 interface SetupProgressBarProps {
   steps: StepType[];
   currentStep: StepType;
@@ -9,8 +11,8 @@ export default function SetupProgressBar({
 }: SetupProgressBarProps) {
   const currentStepIndex = steps.indexOf(currentStep);
 
-  const getStepLabel = (step: StepType): StepLabel<typeof step> => {
-    const labels: StepLabels = {
+  const getStepLabel = (step: StepType) => {
+    const labels = {
       type: "면접 유형",
       method: "면접 방식",
       job: "직무 선택",
@@ -27,25 +29,35 @@ export default function SetupProgressBar({
           <div className="flex items-center">
             <div className="relative">
               <div
-                className={`w-8 h-8 flex items-center justify-center rounded-full ${
-                  index <= currentStepIndex
+                className={`w-8 h-8 flex items-center justify-center rounded-full transition-all duration-300 ${
+                  index < currentStepIndex
                     ? "bg-secondary text-white"
-                    : "bg-gray-300"
+                    : index === currentStepIndex
+                    ? "bg-white border-4 border-secondary"
+                    : "border-4 border-gray-300 text-gray-500"
                 }`}
               >
-                {index + 1}
+                {index < currentStepIndex ? (
+                  <FaCheck size={12} />
+                ) : index === currentStepIndex ? (
+                  <div className="w-2 h-2 bg-secondary rounded-full"></div>
+                ) : null}{" "}
               </div>
-              <div className="absolute top-10 left-1/2 -translate-x-1/2 w-28 text-center">
+              <div className="absolute top-10 left-1/2 -translate-x-1/2 w-28 text-center text-sm font-bold">
                 {getStepLabel(step)}
               </div>
             </div>
 
             {index < steps.length - 1 && (
-              <div
-                className={`h-1 w-20 ${
-                  index < currentStepIndex ? "bg-secondary" : "bg-gray-300"
-                }`}
-              ></div>
+              <div className="relative w-48 h-1">
+                <div className="absolute top-0 left-0 w-full h-full bg-gray-300"></div>
+                <div
+                  className={`absolute top-0 left-0 h-full bg-secondary transition-all duration-500 ease-out`}
+                  style={{
+                    width: index < currentStepIndex ? "100%" : "0%",
+                  }}
+                ></div>
+              </div>
             )}
           </div>
         </div>
