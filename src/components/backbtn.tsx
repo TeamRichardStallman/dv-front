cebc6292f99e9e12626ef48c0589c0c5f67bfe46
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-11-02 17:23:15 +0900

    DV-59 remove: 사용하지 않는 파일 제거

--- src/components/backbtn.tsx
+++
@@ -1,3 +0,0 @@
-export default function BackBtn() {
-  return <div>뒤로가기 버튼 컴포넌트</div>;
-}
--- src/components/defaultbtn.tsx
+++
@@ -1,7 +0,0 @@
-export default function DefaultBtn() {
-  return (
-    <div>
-      기본 버튼(메인색, 서브색, 회색 등 색과 텍스트 넣어서 사용할 예정) 컴포넌트
-    </div>
-  );
-}
