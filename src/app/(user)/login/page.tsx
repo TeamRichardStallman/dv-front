2c9fe0689f39f3325a6e54d16b1e182629d789c2
Danny-Jo <dannyjo.world@gmail.com>
2024-11-03 21:31:13 +0900

    DV-18 feat: 로그인 페이지 UI 업데이트

--- src/app/(user)/login/page.tsx
+++ src/app/(user)/login/page.tsx
@@ -1,9 +1,29 @@
+import Image from "next/image";
+
 export default function Page() {
   return (
     <div className="flex flex-col items-center">
-      <div className="text-3xl">LOGO</div>
-      <h1 className="text-2xl">면접, 그 이상의 기록</h1>
-      <button className="text-2xl">카카오 로그인</button>
+      <div className="relative w-[180.5px] h-[100px] mb-4">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          fill
+          className="object-contain"
+          unoptimized
+          priority
+        />
+      </div>
+      <h1 className="text-2xl font-bold">면접, 그 이상의 기록</h1>
+      <div className="relative w-[200.5px] h-[60px] my-8">
+        <Image
+          src="/kakao_login_large_narrow.png"
+          alt="Kakao Oauth Login Button"
+          fill
+          className="object-contain"
+          unoptimized
+          priority
+        />
+      </div>
     </div>
   );
 }
