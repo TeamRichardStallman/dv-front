32022d8648088f8f78ae38240278953bafa27240
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-11-04 11:08:20 +0900

    DV-65 design: 사용할 수 없는 버튼 비활성화 디자인 추가

--- src/components/settingbtn.tsx
+++ src/components/settingbtn.tsx
@@ -21,30 +21,36 @@ const SettingBtn = ({
 
   return (
     <div
-      className={`relative border rounded-3xl w-[400px] h-[300px] flex items-center justify-center text-center cursor-pointer transition-all duration-300 ${
-        selected || isHovered
+      className={`relative border rounded-3xl w-[400px] h-[300px] flex items-center justify-center text-center transition-all duration-300 ${
+        selected
           ? "bg-primary text-white"
+          : disabled
+          ? "bg-gray-300 text-gray-500"
           : "bg-gray-200 text-black"
-      } ${disabled ? "cursor-not-allowed opacity-50" : ""}`}
+      } ${disabled ? "opacity-60" : ""} ${!disabled ? "cursor-pointer" : ""}`}
       onClick={!disabled ? onClick : undefined}
-      onMouseEnter={() => !disabled && setIsHovered(true)}
-      onMouseLeave={() => !disabled && setIsHovered(false)}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       <div
         className={`font-bold text-4xl tracking-widest transition-transform duration-300 ${
-          isHovered && !selected ? "-translate-y-2" : ""
+          isHovered && !disabled ? "-translate-y-2" : ""
         }`}
       >
         {label}
       </div>
 
       <div
-        className={`absolute bottom-6 text-base font-medium text-black bg-white bg-opacity-90 p-5 rounded-lg shadow-lg transition-all duration-500 transform ${
-          selected || (isHovered && !disabled)
+        className={`absolute bottom-6 text-base font-medium bg-white bg-opacity-90 p-5 rounded-lg shadow-lg transition-all duration-500 transform ${
+          disabled || selected || (isHovered && !disabled)
             ? "opacity-100 translate-y-0"
             : "opacity-0 translate-y-4"
         }`}
-        style={{ width: "90%", minHeight: "4rem" }}
+        style={{
+          width: "90%",
+          minHeight: "4rem",
+          color: disabled ? "gray" : "black",
+        }}
       >
         {descriptionWords.map((word, index) => (
           <span key={index} className="inline-block mr-1">
@@ -52,6 +58,12 @@ const SettingBtn = ({
           </span>
         ))}
       </div>
+
+      {disabled && isHovered && (
+        <div className="absolute inset-0 bg-gray-500 bg-opacity-40 flex items-center justify-center text-black font-bold text-lg rounded-3xl">
+          아직 사용할 수 없습니다.
+        </div>
+      )}
     </div>
   );
 };
