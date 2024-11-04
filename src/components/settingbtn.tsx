b3bc32234f08b0dae439ca87954f181801385ecf
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-11-04 11:14:21 +0900

    DV-65 design: 호버 시 primary 배경색 보이도록 수정

--- src/components/settingbtn.tsx
+++ src/components/settingbtn.tsx
@@ -25,12 +25,14 @@ const SettingBtn = ({
         selected
           ? "bg-primary text-white"
           : disabled
-          ? "bg-gray-300 text-gray-500"
-          : "bg-gray-200 text-black"
-      } ${disabled ? "opacity-60" : ""} ${!disabled ? "cursor-pointer" : ""}`}
+          ? "bg-gray-300 text-gray-500 opacity-60 cursor-not-allowed"
+          : isHovered
+          ? "bg-primary text-white cursor-pointer"
+          : "bg-gray-200 text-black cursor-pointer"
+      }`}
       onClick={!disabled ? onClick : undefined}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
+      onMouseEnter={() => !disabled && setIsHovered(true)}
+      onMouseLeave={() => !disabled && setIsHovered(false)}
     >
       <div
         className={`font-bold text-4xl tracking-widest transition-transform duration-300 ${
@@ -58,12 +60,6 @@ const SettingBtn = ({
           </span>
         ))}
       </div>
-
-      {disabled && isHovered && (
-        <div className="absolute inset-0 bg-gray-500 bg-opacity-40 flex items-center justify-center text-black font-bold text-lg rounded-3xl">
-          아직 사용할 수 없습니다.
-        </div>
-      )}
     </div>
   );
 };
