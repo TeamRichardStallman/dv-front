5961da6ed60d2ac2cbd9d5261ab351f39bac6855
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-11-05 15:42:14 +0900

    DV-105 feat: 현재 보여지는 페이지 메뉴 배경색 추가 및 호버시 글씨 색 변화

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
 const MyPageLayout = ({ children }: { children: React.ReactNode }) => {
+  const currentPath = usePathname();
+
+  const menuItems = [
+    { name: "내 정보 수정", path: "/mypage/edit" },
+    { name: "나의 취업 정보", path: "/mypage/profile" },
+    { name: "면접 평가 확인", path: "/mypage/feedback" },
+    { name: "쿠폰함", path: "/mypage/coupon" },
+    { name: "이용권", path: "/mypage/voucher" },
+    { name: "결제 내역", path: "/mypage/payment-history" },
+  ];
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
@@ -11,54 +25,21 @@ const MyPageLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="flex flex-1">
         <nav className="bg-gray-100 p-6 shadow-lg rounded-lg fixed top-20 left-8 w-56 h-auto">
           <ul className="space-y-4">
-            <li>
-              <Link
-                href="/mypage/edit"
-                className="hover:text-blue-500 font-semibold"
-              >
-                내 정보 수정
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/mypage/profile"
-                className="hover:text-blue-500 font-semibold"
-              >
-                나의 취업 정보
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/mypage/feedback"
-                className="hover:text-blue-500 font-semibold"
-              >
-                면접 평가 확인
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/mypage/coupon"
-                className="hover:text-blue-500 font-semibold"
-              >
-                쿠폰함
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/mypage/voucher"
-                className="hover:text-blue-500 font-semibold"
-              >
-                이용권
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/mypage/payment-history"
-                className="hover:text-blue-500 font-semibold"
-              >
-                결제 내역
-              </Link>
-            </li>
+            {menuItems.map((item) => (
+              <li key={item.path}>
+                <Link href={item.path}>
+                  <span
+                    className={`font-bold block px-4 py-2 rounded-lg ${
+                      currentPath === item.path
+                        ? "bg-primary text-white"
+                        : "hover:text-primary"
+                    }`}
+                  >
+                    {item.name}
+                  </span>
+                </Link>
+              </li>
+            ))}
           </ul>
         </nav>
 
