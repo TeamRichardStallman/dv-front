57b44a9dcc3e8b6b5d46a10b0a98d52596db430a
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-11-08 15:02:31 +0900

    DV-137 feat: 랜딩페이지 캐치프레이즈 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="relative min-h-screen">
-      {/* <div
-        className="absolute inset-0 bg-cover bg-center z-0"
-        style={{ backgroundImage: `url('/backImage.webp')` }}
-      ></div> */}
-
+      <div className="absolute inset-0 opacity-50 z-0"></div>
       <div className="relative z-10 flex flex-col min-h-screen">
         <Header />
-        <main className="relative flex-1 flex flex-col items-center justify-center">
+        <main className="relative flex-1 flex flex-col items-center justify-center text-center p-4">
+          <h1 className="text-5xl font-bold mb-4">Interview King Dev Kim</h1>
+          <p className="text-xl font-medium mb-12 max-w-lg leading-relaxed">
+            면접, 그 이상의 기록
+          </p>
+
           <Link
             href={"/interview"}
-            className="absolute bottom-14 px-6 py-3 bg-secondary font-semibold text-white rounded text-xl"
+            className="absolute bottom-14 px-8 py-4 bg-secondary font-semibold text-white rounded text-xl shadow-lg hover:bg-primary transition-all duration-300"
           >
             시작하기
           </Link>
