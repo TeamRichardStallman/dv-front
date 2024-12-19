ed646b4b256ba6fa3ea8a471002a8718b9446953
andbeyond0331 <andbeyond0331@gmail.com>
2024-12-19 17:33:21 +0900

    fix : 커스텀 모달 버튼 중앙 정렬

--- src/components/modal/custom-modal.tsx
+++ src/components/modal/custom-modal.tsx
@@ -23,19 +23,21 @@ const CustomModal: React.FC<CustomModalProps> = ({
     <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-8 rounded shadow-md">
         <h2 className="text-lg font-semibold">{message}</h2>
-        <div className="mt-4 flex justify-end space-x-4">
-          <button
-            onClick={onClose}
-            className="bg-gray-500 text-white px-4 py-2 rounded"
-          >
-            {cancelButton}
-          </button>
-          <button
-            onClick={onConfirm}
-            className="bg-primary text-white px-4 py-2 rounded"
-          >
-            {confirmButton}
-          </button>
+        <div className="flex items-center justify-center">
+          <div className="mt-4 flex justify-end space-x-4">
+            <button
+              onClick={onClose}
+              className="bg-gray-500 text-white px-4 py-2 rounded"
+            >
+              {cancelButton}
+            </button>
+            <button
+              onClick={onConfirm}
+              className="bg-primary text-white px-4 py-2 rounded"
+            >
+              {confirmButton}
+            </button>
+          </div>
         </div>
       </div>
     </div>
