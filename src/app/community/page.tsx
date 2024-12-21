96bf0252cd1308e32028499a7a2bdb20a5c9ecfe
andbeyond0331 <mjpark1329@naver.com>
2024-12-22 02:41:19 +0900

    feat : 커스텀 모달 여백 제거, 구독 상태 불러오기, 게시글 이미지 업로드, 게시글 생성, 구독 취소 / 삭제, 피드 UI 개선, 이미지 폭 잘리지 않게 개선

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,10 +1,19 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import axios from "axios";
 import { setUrl } from "@/utils/setUrl";
-import { postData } from "@/data/postData";
 import PostModal from "@/components/post-modal";
+import NoContent from "@/components/no-content";
+
+export interface PostCreateRequestDto {
+  jobKoreanName: string;
+  content: string;
+  s3ImageUrl: string | null;
+  interviewId: number | null;
+  overallEvaluationId: number | null;
+  postType: string;
+}
 
 interface GetUserResponse {
   data: {
@@ -20,34 +29,59 @@ interface GetUserResponse {
     birthdate: string;
   };
 }
+
+interface PostCreateResponse {
+  data: Post;
+}
+
+interface GetPostResponse {
+  data: PostResponse;
+}
+
+interface PostList {
+  posts: Post[];
+}
+
+interface Post {
+  postId: number;
+  authorId: number;
+  authorNickname: string;
+  authorProfileUrl: string;
+  jobName: string;
+  jobNameKorean: string;
+  content: string;
+  s3ImageUrl: string;
+  interview: InterviewDetails;
+  evaluation: EvaluationDetailType;
+  postType: string;
+  generatedAt: Date;
+}
+
+export interface PostResponse {
+  data: PostList;
+}
+
 const apiUrl = `${setUrl}`;
 
 const CommunityPage = () => {
-  const posts = postData.data.posts;
+  const [posts, setPosts] = useState<Post[]>([]);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [user, setUser] = useState<GetUserResponse["data"] | null>(null);
   const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState<any[]>([]);
   const [recommendedChannels, setRecommendedChannels] = useState([
-    { name: "네이버", tag: "기업", isSubscribed: false },
-    { name: "카카오", tag: "기업", isSubscribed: false },
-    { name: "백엔드", tag: "직무", isSubscribed: false },
-    { name: "프론트엔드", tag: "직무", isSubscribed: false },
-    { name: "UX/UI", tag: "직무", isSubscribed: false },
+    { name: "백엔드", tag: "직무", isSubscribed: false, jobId: 1 },
+    { name: "프론트엔드", tag: "직무", isSubscribed: false, jobId: 2 },
+    { name: "인프라", tag: "직무", isSubscribed: false, jobId: 3 },
+    { name: "인공지능", tag: "직무", isSubscribed: false, jobId: 4 },
   ]);
+  const [activeFeed, setActiveFeed] = useState<"USER" | "SUBSCRIPTION">(
+    "SUBSCRIPTION"
+  );
 
   const toggleMenu = () => {
     setIsMenuOpen((prev) => !prev);
-  };
-
-  const handleSubscribe = (index: number) => {
-    setRecommendedChannels((prevChannels) =>
-      prevChannels.map((channel, i) =>
-        i === index
-          ? { ...channel, isSubscribed: !channel.isSubscribed }
-          : channel
-      )
-    );
   };
 
   const openModal = async () => {
@@ -66,20 +100,218 @@ const CommunityPage = () => {
     }
   };
 
+  interface SubscriptionListResponse {
+    data: Subscription[];
+  }
+
+  interface Subscription {
+    subscriptionId: number;
+    userId: number;
+    jobId: number;
+    subscribedAt: Date;
+  }
+
+  useEffect(() => {
+    const fetchSubscriptionStatus = async () => {
+      try {
+        const response = await axios.get<SubscriptionListResponse>(
+          `${apiUrl}/subscription/user`,
+          {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        const subscribedJobs = response.data.data;
+        setRecommendedChannels((prev) =>
+          prev.map((channel) => ({
+            ...channel,
+            isSubscribed: subscribedJobs.some((e) => e.jobId === channel.jobId),
+          }))
+        );
+      } catch (error) {
+        console.error("구독 상태 불러오기 실패:", error);
+      }
+    };
+    if (activeFeed === "SUBSCRIPTION") {
+      fetchSubscriptionPosts();
+    } else if (activeFeed === "USER") {
+      fetchMyPosts();
+    }
+
+    fetchSubscriptionStatus();
+  }, []);
+
   const closeModal = () => {
     setIsModalOpen(false);
   };
 
-  const handlePostSubmit = (postData: {
-    jobKoreanName: string;
-    content: string;
-    s3ImageUrl: string;
-    interviewId: number;
-    overallEvaluationId: number;
-    postType: string;
-  }) => {
-    console.log("작성된 게시글 데이터:", postData);
-    closeModal();
+  const handlePostSubmit = async (
+    postData: PostCreateRequestDto,
+    file: File | null
+  ) => {
+    try {
+      const response = await axios.post<PostCreateResponse>(
+        `${apiUrl}/post`,
+        {
+          jobKoreanName: postData.jobKoreanName,
+          content: postData.content,
+          s3ImageUrl: postData.s3ImageUrl ? postData.s3ImageUrl : null,
+          postType: "POST",
+        },
+        {
+          withCredentials: true,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
+      if (response.data && response.data.data) {
+        const data = response.data.data;
+        if (file) {
+          const preSignedResponse = await axios.get<GetPreSignedUrlResponse>(
+            `${apiUrl}/file/post-image/${encodeURIComponent(file.name)}/${
+              data.postId
+            }/upload-url`,
+            {
+              withCredentials: true,
+              headers: { "Content-Type": "application/json" },
+            }
+          );
+
+          const presignedUrl = preSignedResponse.data.data.preSignedUrl;
+          if (presignedUrl) {
+            fetch(presignedUrl, {
+              method: "PUT",
+              headers: { "Content-Type": file.type },
+              body: file,
+            }).catch((error) => {
+              console.error(error);
+            });
+
+            const updatedPost = await axios.put<PostCreateResponse>(
+              `${apiUrl}/post/image`,
+              {
+                imageUrl: preSignedResponse.data.data.objectKey,
+                postId: data.postId,
+              },
+              {
+                withCredentials: true,
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+          }
+        }
+      }
+      fetchMyPosts();
+    } catch (error) {
+      console.error("게시글 작성 실패:", error);
+    }
+  };
+
+  const handleSearch = async () => {
+    if (searchQuery.trim() === "") return;
+
+    try {
+      const response = await axios.get<PostResponse>(`${apiUrl}/post/search`, {
+        params: { keyword: searchQuery },
+        withCredentials: true,
+        headers: { "Content-Type": "application/json" },
+      });
+      const { data } = response.data;
+
+      setSearchResults(data.posts);
+    } catch (error) {
+      console.error("게시글 검색 실패:", error);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  const fetchMyPosts = async () => {
+    try {
+      const response = await axios.get<PostResponse>(`${apiUrl}/post/user`, {
+        withCredentials: true,
+      });
+      setPosts(response.data.data.posts);
+      setSearchResults([]);
+      setActiveFeed("USER");
+    } catch (error) {
+      console.error("내 피드 가져오기 실패:", error);
+    }
+  };
+
+  const fetchSubscriptionPosts = async () => {
+    try {
+      const response = await axios.get<PostResponse>(
+        `${apiUrl}/post/subscription`,
+        {
+          withCredentials: true,
+          params: { page: 0 },
+        }
+      );
+      setPosts(response.data.data.posts);
+      setSearchResults([]);
+      setActiveFeed("SUBSCRIPTION");
+    } catch (error) {
+      console.error("구독 피드 가져오기 실패:", error);
+    }
+  };
+
+  const createSubscription = async (jobId: number, index: number) => {
+    try {
+      await axios.post(
+        `${apiUrl}/subscription`,
+        { jobId },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setRecommendedChannels((prev) =>
+        prev.map((channel, i) =>
+          i === index ? { ...channel, isSubscribed: true } : channel
+        )
+      );
+    } catch (error) {
+      console.error("구독 생성 실패:", error);
+    }
+  };
+
+  const deactivateSubscription = async (jobId: number, index: number) => {
+    try {
+      await axios.delete(`${apiUrl}/subscription/${jobId}`, {
+        withCredentials: true,
+      });
+      setRecommendedChannels((prev) =>
+        prev.map((channel, i) =>
+          i === index ? { ...channel, isSubscribed: false } : channel
+        )
+      );
+    } catch (error) {
+      console.error("구독 취소 실패:", error);
+    }
+  };
+
+  const handleSubscribeClick = (
+    jobId: number,
+    isSubscribed: boolean,
+    index: number
+  ) => {
+    if (isSubscribed) {
+      deactivateSubscription(jobId, index);
+    } else {
+      createSubscription(jobId, index);
+    }
   };
 
   return (
@@ -89,6 +321,7 @@ const CommunityPage = () => {
           type="text"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyDown={handleKeyDown}
           placeholder="검색어를 입력하세요."
           className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
@@ -102,13 +335,21 @@ const CommunityPage = () => {
                 key={index}
                 className="flex justify-between items-center p-2 rounded-md mb-2"
               >
-                <span className="font-bold">{channel.name}</span>
-                <div className="flex items-center">
-                  <span className="text-sm font-semibold bg-primary text-white px-3 py-1 rounded-md mr-2">
-                    {channel.tag}
+                <div>
+                  <span className="font-bold mr-2">{channel.name}</span>
+                  <span className="px-3 py-1 bg-blue-100 text-blue-600 text-xs font-medium rounded-full mr-2">
+                    직무
                   </span>
+                </div>
+                <div className="flex items-center">
                   <button
-                    onClick={() => handleSubscribe(index)}
+                    onClick={() =>
+                      handleSubscribeClick(
+                        channel.jobId,
+                        channel.isSubscribed,
+                        index
+                      )
+                    }
                     className={`text-sm font-semibold rounded-md px-3 py-1 ${
                       channel.isSubscribed
                         ? "bg-gray-500 text-white"
@@ -125,47 +366,132 @@ const CommunityPage = () => {
       </div>
 
       <div className="max-w-3xl mx-auto px-4 py-8">
+        <div className="flex gap-2 mb-4">
+          <button
+            onClick={fetchSubscriptionPosts}
+            className={`px-4 py-2 rounded-md font-bold ${
+              activeFeed === "SUBSCRIPTION"
+                ? "bg-blue-500 text-white"
+                : "bg-gray-300"
+            }`}
+          >
+            구독 피드
+          </button>
+          <button
+            onClick={fetchMyPosts}
+            className={`px-4 py-2 rounded-md font-bold ${
+              activeFeed === "USER" ? "bg-blue-500 text-white" : "bg-gray-300"
+            }`}
+          >
+            내 피드
+          </button>
+        </div>
         <div className="flex flex-col gap-6">
-          {posts.map((post) => (
-            <div
-              key={post.postId}
-              className="bg-white shadow-md rounded-lg overflow-hidden border"
-            >
-              <div className="flex items-center justify-between p-4">
-                <div className="flex items-center">
-                  <div className="relative w-10 h-10 mr-3">
+          {(searchResults && searchResults.length > 0) ||
+          (posts && posts.length > 0) ? (
+            (searchResults.length > 0 ? searchResults : posts).map((post) =>
+              post.s3ImageUrl ? (
+                <div
+                  key={post.postId}
+                  className="bg-white shadow-md rounded-lg overflow-hidden border"
+                >
+                  <div className="flex items-center justify-between p-4">
+                    <div className="flex items-center">
+                      <div className="relative w-10 h-10 mr-3">
+                        <Image
+                          src={
+                            post.authorProfileUrl
+                              ? `https://ktb-8-dev-bucket.s3.ap-northeast-2.amazonaws.com/${post.authorProfileUrl}`
+                              : "/profile-img.png"
+                          }
+                          alt={`${post.authorNickname}'s profile`}
+                          layout="fill"
+                          objectFit="cover"
+                          className="rounded-full"
+                          onError={() =>
+                            console.error(
+                              "이미지 로드 실패. URL:",
+                              post.authorProfileUrl
+                            )
+                          }
+                        />
+                      </div>
+                      <span className="text-sm font-semibold">
+                        {post.authorNickname}
+                      </span>
+                    </div>
+                    <span className="px-3 py-1 bg-blue-100 text-blue-600 text-sm font-medium rounded-full">
+                      {post.jobNameKorean}
+                    </span>
+                  </div>
+                  <div className="relative w-full h-96">
                     <Image
-                      src={post.authorProfileUrl}
-                      alt={`${post.authorNickname}'s profile`}
+                      src={post.s3ImageUrl}
+                      alt={`Post ${post.postId}`}
                       layout="fill"
-                      objectFit="cover"
-                      className="rounded-full"
+                      objectFit="contain"
                     />
                   </div>
-                  <span className="text-sm font-semibold">
-                    {post.authorNickname}
-                  </span>
+                  <div className="p-4">
+                    <p className="text-gray-800 mb-3">{post.content}</p>
+                    <span className="text-sm text-gray-500">
+                      {new Date(post.generatedAt).toLocaleString()}
+                    </span>
+                  </div>
                 </div>
-                <span className="px-3 py-1 bg-blue-100 text-blue-600 text-sm font-medium rounded-full">
-                  {post.jobNameKorean}
-                </span>
-              </div>
-              <div className="relative w-full h-96">
-                <Image
-                  src={post.s3ImageUrl}
-                  alt={`Post ${post.postId}`}
-                  layout="fill"
-                  objectFit="cover"
-                />
-              </div>
-              <div className="p-4">
-                <p className="text-gray-800 mb-3">{post.content}</p>
-                <span className="text-sm text-gray-500">
-                  {new Date(post.generatedAt).toLocaleString()}
-                </span>
-              </div>
+              ) : (
+                <div
+                  key={post.postId}
+                  className="bg-white shadow-md rounded-lg overflow-hidden border"
+                >
+                  <div className="flex items-center justify-between p-4">
+                    <div className="flex items-center">
+                      <div className="relative w-10 h-10 mr-3">
+                        <Image
+                          src={
+                            post.authorProfileUrl
+                              ? `https://ktb-8-dev-bucket.s3.ap-northeast-2.amazonaws.com/${post.authorProfileUrl}`
+                              : "/profile-img.png"
+                          }
+                          alt={`${post.authorNickname}'s profile`}
+                          layout="fill"
+                          objectFit="cover"
+                          className="rounded-full"
+                          onError={() =>
+                            console.error(
+                              "이미지 로드 실패. URL:",
+                              post.authorProfileUrl
+                            )
+                          }
+                        />
+                      </div>
+                      <span className="text-sm font-semibold">
+                        {post.authorNickname}
+                      </span>
+                    </div>
+                    <span className="px-3 py-1 bg-blue-100 text-blue-600 text-sm font-medium rounded-full">
+                      {post.jobNameKorean}
+                    </span>
+                  </div>
+
+                  <div className="p-4">
+                    <p className="text-gray-800 mb-3">{post.content}</p>
+                    <span className="text-sm text-gray-500">
+                      {new Date(post.generatedAt).toLocaleString()}
+                    </span>
+                  </div>
+                </div>
+              )
+            )
+          ) : (
+            <div className="bg-white shadow-md rounded-lg overflow-hidden border pb-16">
+              <NoContent
+                text={
+                  activeFeed === "SUBSCRIPTION" ? "구독 정보가" : "내 게시글이"
+                }
+              />
             </div>
-          ))}
+          )}
         </div>
       </div>
 
@@ -186,7 +512,8 @@ const CommunityPage = () => {
                 />
               </div>
             </div>
-            <div className="flex items-center">
+            {/* 잠시 안녕 */}
+            {/* <div className="flex items-center">
               <span className="mr-3 text-black font-bold">면접 공유</span>
               <div className="relative w-12 h-12 flex items-center justify-center rounded-full bg-white">
                 <Image
@@ -196,8 +523,8 @@ const CommunityPage = () => {
                   height={36}
                 />
               </div>
-            </div>
-            <div className="flex items-center">
+            </div> */}
+            {/* <div className="flex items-center">
               <span className="mr-3 text-black font-bold">피드백 공유</span>
               <div className="relative w-12 h-12 flex items-center justify-center rounded-full bg-white">
                 <Image
@@ -207,12 +534,12 @@ const CommunityPage = () => {
                   height={24}
                 />
               </div>
-            </div>
+            </div> */}
           </div>
         )}
         <button
           onClick={toggleMenu}
-          className="w-15 h-15 flex items-center justify-center rounded-full shadow-md"
+          className="w-15 h-15 flex items-center justify-center rounded-full"
         >
           <Image
             src="/icons/icon-plus-circle.svg"
