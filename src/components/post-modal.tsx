e059bc763ad13d36fb877955841a960b248898fe
andbeyond0331 <mjpark1329@naver.com>
2024-12-22 02:48:10 +0900

    feat : 클라우드 -> 인프라로 용어 통일, 파일 업로드, 게시글 작성 처리

--- src/components/post-modal.tsx
+++ src/components/post-modal.tsx
@@ -1,19 +1,14 @@
 "use client";
 import React, { FC, useState } from "react";
 import Image from "next/image";
+import { PostCreateRequestDto } from "@/app/community/page";
 
 interface PostModalProps {
   isOpen: boolean;
   onClose: () => void;
-  onSubmit: (postData: {
-    jobKoreanName: string;
-    content: string;
-    s3ImageUrl: string;
-    interviewId: number;
-    overallEvaluationId: number;
-    postType: string;
-  }) => void;
+  onSubmit: (postData: PostCreateRequestDto, file: File | null) => void;
   user: {
+    userId: number;
     nickname: string;
     s3ProfileImageUrl: string;
   } | null;
@@ -23,110 +18,128 @@ const PostModal: FC<PostModalProps> = ({ isOpen, onClose, onSubmit, user }) => {
   const [content, setContent] = useState("");
   const [jobKoreanName, setJobKoreanName] = useState("");
   const [uploadedImage, setUploadedImage] = useState<string | null>(null);
+  const [file, setFile] = useState<File | null>(null);
+  const [loading] = useState(false);
+  const supportedExtensions = ["image/jpeg", "image/jpg", "image/png"];
 
-  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleImageSet = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
+      const selectedFile = e.target.files[0];
+
+      if (!supportedExtensions.includes(selectedFile.type)) {
+        alert("JPG, JPEG, PNG 파일만 업로드 가능합니다.");
+        return;
+      }
+
+      setFile(selectedFile);
+
       const reader = new FileReader();
       reader.onload = (e) => {
         if (e.target?.result) {
           setUploadedImage(e.target.result.toString());
         }
       };
-      reader.readAsDataURL(e.target.files[0]);
+      reader.readAsDataURL(selectedFile);
     }
   };
 
-  const removeUploadedImage = () => {
-    setUploadedImage(null);
+  const handleRealSubmit = async () => {
+    try {
+      onSubmit(
+        {
+          jobKoreanName:
+            jobKoreanName === "클라우드" ? "인프라" : jobKoreanName,
+          content,
+          s3ImageUrl: null,
+          interviewId: null,
+          overallEvaluationId: null,
+          postType: "POST",
+        },
+        file
+      );
+      setContent("");
+      setJobKoreanName("");
+      setUploadedImage(null);
+      setFile(null);
+      onClose();
+    } catch (error) {
+      console.error("게시글 작성 실패:", error);
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (jobKoreanName) {
+      if (content && content.length > 0) {
+        handleRealSubmit();
+      } else {
+        alert("내용을 입력해주세요.");
+        return;
+      }
+    } else {
+      alert("직무를 선택해주세요.");
+      return;
+    }
   };
 
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white rounded-lg p-6 w-[600px] h-auto shadow-lg">
-        <div className="flex justify-between items-center mb-4">
-          {user && (
-            <div className="flex items-center">
-              <div className="relative w-12 h-12 rounded-full overflow-hidden mr-4">
-                <Image
-                  src={user.s3ProfileImageUrl}
-                  alt={`${user.nickname}의 프로필`}
-                  layout="fill"
-                  objectFit="cover"
-                  priority
-                />
-              </div>
-              <span className="font-bold text-lg">{user.nickname}</span>
+      <div className="bg-white rounded-lg p-6 w-[600px] shadow-lg">
+        <div className="mb-4 flex justify-between items-center">
+          <div className="flex items-center">
+            <div className="w-12 h-12 rounded-full overflow-hidden">
+              <Image
+                src={user?.s3ProfileImageUrl || "/profile-img.png"}
+                alt="Profile"
+                width={48}
+                height={48}
+              />
             </div>
-          )}
+            <span className="ml-2 font-bold">{user?.nickname}</span>
+          </div>
           <select
             value={jobKoreanName}
             onChange={(e) => setJobKoreanName(e.target.value)}
-            className="p-2 border border-gray-300 rounded-md"
+            className="p-2 border rounded"
           >
-            <option value="">직무를 선택하세요</option>
+            <option value="">직무 선택</option>
             <option value="백엔드">백엔드</option>
             <option value="프론트엔드">프론트엔드</option>
-            <option value="클라우드">클라우드</option>
+            <option value="클라우드">인프라</option>
             <option value="인공지능">인공지능</option>
           </select>
         </div>
-        <div className="mb-4">
-          <input
-            type="file"
-            accept="image/*"
-            onChange={handleImageUpload}
-            className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
-          />
-          {uploadedImage && (
-            <div className="mt-4 relative w-full h-96 rounded-md overflow-hidden border">
-              <Image
-                src={uploadedImage}
-                alt="Uploaded Preview"
-                layout="fill"
-                objectFit="cover"
-              />
-              <button
-                onClick={removeUploadedImage}
-                className="absolute top-2 right-2 w-8 h-8 bg-black text-white flex items-center justify-center rounded-full shadow"
-              >
-                <span className="text-lg font-bold">&times;</span>
-              </button>
-            </div>
-          )}
-        </div>
+        <input type="file" onChange={handleImageSet} accept="image/*" />
+        {uploadedImage && (
+          <div className="relative mt-4 w-full h-64">
+            <Image
+              src={uploadedImage}
+              alt="Preview"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        )}
         <textarea
           value={content}
           onChange={(e) => setContent(e.target.value)}
-          placeholder="내용을 입력하세요.."
-          className="w-full p-2 border font-medium border-gray-300 rounded-md resize-none h-32 focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4"
+          placeholder="내용을 입력하세요"
+          className="w-full mt-4 p-2 border rounded"
         />
-        <div className="flex justify-end mt-4 space-x-2">
+        <div className="flex justify-end mt-4">
           <button
+            className="px-4 py-2 bg-gray-400 text-white rounded mr-2"
             onClick={onClose}
-            className="px-4 py-2 bg-gray-400 font-bold text-white rounded-lg hover:bg-gray-500"
           >
             취소
           </button>
           <button
-            onClick={() => {
-              onSubmit({
-                jobKoreanName,
-                content,
-                s3ImageUrl: uploadedImage || "https://example.com/image.jpg",
-                interviewId: 1,
-                overallEvaluationId: 2,
-                postType: "INTERVIEW",
-              });
-              setContent("");
-              setJobKoreanName("");
-              setUploadedImage(null);
-              onClose();
-            }}
-            className="px-4 py-2 bg-primary font-bold text-white rounded-lg hover:bg-blue-600"
+            className="px-4 py-2 bg-blue-500 text-white rounded"
+            onClick={handleSubmit}
+            disabled={loading}
           >
-            작성 완료
+            {loading ? "업로드 중..." : "작성 완료"}
           </button>
         </div>
       </div>
